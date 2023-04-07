e0744efe701e92b6244e3e014644703771f3eaa2
Neel028 <neelkoshiya028@gmail.com>
2023-04-07 21:38:58 +0530

    finish

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import './app.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let [state, setstate] = useState(0)
+
+    useEffect(() => {
+        const time = setInterval(() => {
+            setstate(++state)
+        }, 1000)
+
+
+        return () => {
+            clearInterval(time)
+        }
+    },[state])
+
+
+    return (
+        <div>
+            <h1>{state}</h1>
+        </div>
+    )
+
 }
 
 export default App;
